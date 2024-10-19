60a798b12aab47d8fee6d90c4e0b85a34e4f629d
duttaaindrila23 <aindriladutta392@gmail.com>
2024-10-19 19:59:35 +0530

    changed spelling

--- Project/server/utils/db.js
+++ Project/server/utils/db.js
@@ -11,7 +11,7 @@ async function connectDb(){
                 throw new Error("MONGO_URI is not defined in the environment variables");
             }
             await mongoose.connect(mongodburl);
-            console.log("Database conected");
+            console.log("Database connected");
             console.log(mongoose.connection.host);    
             
         }
