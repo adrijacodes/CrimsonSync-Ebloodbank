f0e28ba10fab8e2b53b333964d861443c73d41cf
duttaaindrila23 <aindriladutta392@gmail.com>
2024-10-20 02:55:18 +0530

    made a few changes in authentication

--- Project/server/controllers/authcontroller.js
+++ Project/server/controllers/authcontroller.js
@@ -19,3 +19,6 @@ export const register=async(req,res,next)=>{
     }
     }
     
+export const login=async (req,res,next) => {
+    const {email,password}=req.body
+}
--- Project/server/routes/authRoute.js
+++ Project/server/routes/authRoute.js
@@ -1,7 +1,7 @@
 import express from "express"
-import {register} from "../controllers/authcontroller.js"
+import {register,login} from "../controllers/authcontroller.js"
 const router=express.Router()
 
 router.post('/register',register)
-
+router.post('/login',login)
 export default router
