0780abbe9d83a5f9528df136ebf4e4512dd93919
Debjit Dey <debjitdeylm30@gmail.com>
2025-04-12 22:19:44 +0530

    node modules gone

--- Project/client/src/Components/utils/BaseUrl.js
+++
@@ -1,9 +0,0 @@
-export const baseURL ="http://localhost:5000";
-
-export const config ={
-    Headers: {
-        "content-Type": "application/json",
-    },
-    withCredentials: true
-
-};
--- Project/server/server.js
+++
@@ -1,9 +0,0 @@
-import {app} from "./src/app.js"
-
-
-
-
-app.listen(8001,()=>{
-    console.log(`Server started at port succesfully 8001`);
-    
-})
