4380cca771165edb7ddacf096febaad2ad974254
duttaaindrila23 <aindriladutta392@gmail.com>
2025-05-05 21:55:49 +0530

    notification model added, logic for all notifications implemented

---
+++ Project/server/src/models/notificationsModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    bloodRequestId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "BloodRequest",
+      required: true,
+    },
+    message: { type: String, required: true },
+    status: {
+      type: String,
+      enum: ["active", "seen", "accepted", "rejected", "cancelled"],
+      default: "active",
+    },
+    type: {
+      type: String,
+      enum: ["info", "acknowledgement"],
+      default: "info",
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true } 
+);
+
+const Notification = mongoose.model("Notifications", notificationSchema);
+export default Notification;
