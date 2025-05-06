a9183632af83f63c34a7ce90376a55a7bc863bf6
duttaaindrila23 <aindriladutta392@gmail.com>
2025-05-07 02:00:17 +0530

    submit eligibility fomr

--- Project/server/src/routes/bloodRequestSearch.js
+++ Project/server/src/routes/bloodRequestSearch.js
@@ -1,5 +1,8 @@
 import express from "express";
-import { createBloodRequest } from "../controllers/searchdonorcontroller.js";
+import {
+  createBloodRequest,
+  submitEligibilityForm,
+} from "../controllers/searchdonorcontroller.js";
 import verifyUserToken1 from "../middlewares/userAuth.middlewares.js";
 import {
   adminRolecheck,
@@ -7,8 +10,14 @@ import {
 } from "../middlewares/userRoleChecking.js";
 const router = express.Router();
 
+//creating new search blood request
 router
   .route("/search-blood")
   .post(verifyUserToken1, userRolecheck, createBloodRequest);
+
+//submitting eligibility form
+router
+  .route("/eligibility-form/:notificationId")
+  .post(verifyUserToken1, userRolecheck, submitEligibilityForm);
 
 export default router;
