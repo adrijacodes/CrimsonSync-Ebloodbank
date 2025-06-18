c0b9808075f626dcd7047f38cc448e3170e55d99
duttaaindrila23 <aindriladutta392@gmail.com>
2025-06-18 08:09:59 +0530

    time changed

--- Project/server/src/crons/bloodRequestScheduler.js
+++ Project/server/src/crons/bloodRequestScheduler.js
@@ -5,7 +5,7 @@ import User from "../models/userModel.js";
 import EligibilityForm from "../models/eligibilityFormModel.js";
 
 // Cron runs every 2 minutes
-cron.schedule("*/1 * * * *", async () => {
+cron.schedule("*/*/30 * * * * *", async () => {
   try {
     console.log("🩸 Cron job triggered to process pending blood requests.");
 
